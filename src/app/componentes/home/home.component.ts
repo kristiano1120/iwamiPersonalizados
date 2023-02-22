ca7643afd9045f85967bd1fcf2ec58239b73eb50
kristiano1120 <kristiano1120@hotmail.com>
2023-02-21 21:19:12 -0500

    FORM

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
 
   constructor() { }
@@ -12,16 +14,19 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  loginUser = new FormControl();
+
+
   loginForm = true;
-
+  
   modal = document.getElementById('login');
+  //When the user clicks anywhere outside of the modal, close it
+  /* document.window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  } */
 
-  // When the user clicks anywhere outside of the modal, close it
-  // window.onclick = function(event) {
-  //     if (event.target == modal) {
-  //         modal.style.display = "none";
-  //     }
-  // }
 
   login = {
     user: '',
